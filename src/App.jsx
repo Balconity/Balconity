9a0d1d36fc0fdff50fbcca7d25027627f5fbc14e
Tomi Levkus <tomislav@pickpack.hr>
2025-10-07 20:51:22 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className="balconity">BALCONITY</h1>
+      <p className="comming-soon">
+        comming soon ...
+      </p>
+    </>
+  )
+}
+
+export default App
